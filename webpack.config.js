e2e5016e599441152b8654c340f9f6f0c812e621
Vadym Markov <trezub123456@gmail.com>
2025-07-27 19:00:18 +0300

    Configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/main.ts',
@@ -34,6 +35,11 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'style.css', to: '.' }
+            ],
         }),
     ],
 };
